b5370e3bf0ba1537e1a99897617b9b2c19f14f97
Sofiia Lushchanets <sofiya067@gmail.com>
2017-01-03 17:34:45 +0200

    added click sound to buttons

--- CalculatorProgect/InputController.swift
+++ CalculatorProgect/InputController.swift
@@ -7,35 +7,54 @@
 //
 
 import UIKit
+import AVFoundation
 
 class InputController: UIViewController, InputInterface {
     
     var buttonDidPress: ((_ operation: String)->())? = nil
     
+    var audioPlayer = AVAudioPlayer()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "click_one", ofType: "wav")!))
+            audioPlayer.prepareToPlay()
+        }
+        catch {
+        print(error)
+        }
+    }
     
     //MARK: - Actions
     
     @IBAction func touchDigitAction(_ sender: UIButton) {
+        audioPlayer.play()
         buttonDidPress?(sender.currentTitle!)
     }
     
     @IBAction func binaryOperationAction(_ sender: UIButton) {
+        audioPlayer.play()
         buttonDidPress?(sender.currentTitle!)
     }
     
     @IBAction func unaryOperationAction(_ sender: UIButton) {
+        audioPlayer.play()
         buttonDidPress?(sender.currentTitle!)
     }
     
     @IBAction func equalsAction(_ sender: UIButton) {
+        audioPlayer.play()
         buttonDidPress?(sender.currentTitle!)
     }
     
     @IBAction func clearAction(_ sender: UIButton) {
+        audioPlayer.play()
         buttonDidPress?(sender.currentTitle!)
     }
     
     @IBAction func dotAction(_ sender: UIButton) {
+        audioPlayer.play()
         buttonDidPress?(sender.currentTitle!)
     }
 }
