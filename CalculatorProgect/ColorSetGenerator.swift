1d84ebd7ae2dc964b67515fe544fcd134367da4a
Sofiia Lushchanets <sofiya067@gmail.com>
2017-01-06 00:32:53 +0200

    -implemented table view data source methods -generated set of colors for one theme -set’s name and icon are presented in table view row

--- CalculatorProgect/ColorSetGenerator.swift
+++ CalculatorProgect/ColorSetGenerator.swift
@@ -19,13 +19,16 @@ class ColorSetGenerator: NSObject {
     }
 
     private func generateColorSetOne() -> ColorSet {
+        
         let colorSet = ColorSet()
         
         colorSet.colorA = UIColor.init(colorLiteralRed: 40.0 / 255.0, green: 110.0 / 255.0, blue: 131.0 / 255.0, alpha: 0.92)
         colorSet.colorB = UIColor.init(colorLiteralRed: 70.0 / 255.0, green: 149.0 / 255.0, blue: 161.0 / 255.0, alpha: 0.89)
         colorSet.colorC = UIColor.init(colorLiteralRed: 39.0 / 255.0, green: 105.0 / 255.0, blue: 105.0 / 255.0, alpha: 0.9)
         colorSet.colorD = UIColor.init(colorLiteralRed: 230.0 / 255.0, green: 229.0 / 255.0, blue: 231.0 / 255.0, alpha: 1.0)
-
+        colorSet.setIcon = "turquiseSet"
+        colorSet.setName = "Turquise"
+        
         return colorSet
     }
 //    private func generateColorSetTwo() -> ColorSet {
--- CalculatorProgect/ThemesTableViewController.swift
+++ CalculatorProgect/ThemesTableViewController.swift
@@ -10,10 +10,11 @@ import UIKit
 
 class ThemesTableViewController: UITableViewController {
     
+    var arrayWithColorSets = Array<ColorSet>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        arrayWithColorSets = ColorSetGenerator.sharedInstance.generateColorSetsArray()
     }
 
     
@@ -22,23 +23,26 @@ class ThemesTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return arrayWithColorSets.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellIdentifier", for: indexPath)
+        
+        let currentSet = arrayWithColorSets[indexPath.row]
+        cell.textLabel?.text = currentSet.setName
+        cell.imageView?.image = UIImage.init(named: currentSet.setIcon)
+        
         return cell
     }
-    */
+ 
 
     /*
     // Override to support conditional editing of the table view.
