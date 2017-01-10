54c7a078258d61c6ba044f0d9bb9c4829f48ec8e
Sofiia Lushchanets <sofiya067@gmail.com>
2017-01-10 16:47:23 +0200

    -added checkmarks to tableview -chosen row check mark is being saved to userdefaults -changed backgrounds of tableview and its cell

--- CalculatorProgect/ThemesTableViewController.swift
+++ CalculatorProgect/ThemesTableViewController.swift
@@ -9,8 +9,10 @@
 import UIKit
 
 class ThemesTableViewController: UITableViewController {
+    @IBOutlet var themesTableView: UITableView!
     
     var arrayWithColorSets = Array<ColorSet>()
+    var rowIsChecked = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,9 +29,8 @@ class ThemesTableViewController: UITableViewController {
         return arrayWithColorSets.count
     }
 
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
+
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellIdentifier", for: indexPath) as! AppThemeTableViewCell
         
         let currentSet = arrayWithColorSets[indexPath.row]
@@ -37,63 +38,31 @@ class ThemesTableViewController: UITableViewController {
         cell.colorSetImage?.image = UIImage.init(named: currentSet.setIcon)
         cell.colorSetImage?.layer.cornerRadius = 25//making imageView round
         cell.colorSetImage?.layer.masksToBounds = true
-
+        
+        //the code below - gets info on Checkmark and presents it in table view
+        let selectedRow = UserDefaults.standard.integer(forKey: "chosenColorSet")
+        rowIsChecked = UserDefaults.standard.bool(forKey: "rowIsChecked")
+        
+        if (indexPath.row == selectedRow) && rowIsChecked  {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
+        
         return cell
     }
  
     // MARK: - Table view delegate
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        UserDefaults.standard.set(indexPath.row, forKey: "chosenColorSet")
+        
+        tableView.deselectRow(at: indexPath, animated: true)//highlights for a second
+        
+        if let cell = tableView.cellForRow(at: indexPath) {
+            cell.accessoryType = .checkmark
+            UserDefaults.standard.set(indexPath.row, forKey: "chosenColorSet")
+            UserDefaults.standard.set(true, forKey: "rowIsChecked")
+            tableView.reloadData()
+        }
     }
-
-    
-    
-    
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
